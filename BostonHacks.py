c003931bd5d2b1a9015419dc586f3c9f7e36cdf8
Pranav Raikundalia <pranavraikundalia@dhcp-wifi-8021x-168-122-12-89.bu.edu>
2017-10-29 03:33:13 -0400

    backend done

--- BostonHacks.py
+++ BostonHacks.py
@@ -2,7 +2,18 @@ from flask import Flask, Response, request, render_template, redirect, url_for, 
 from flaskext.mysql import MySQL
 import datetime, os, shutil
 from twilio.rest import *
+import datetime,time
 
+
+app = Flask(__name__, static_folder="static")
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'I(E(ream7861'
+app.config['MYSQL_DATABASE_DB'] = 'BostonHacks'
+app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+mysql.init_app(app)
+conn = mysql.connect()
+cursor = conn.cursor()
 app = Flask(__name__)
 
 # Your Account SID from twilio.com/console
@@ -18,10 +29,88 @@ app = Flask(__name__)
 #       body="Hello from Python!")
 
 #print(message.sid)
+def extractData(cursor):
+    data = []
+    for item in cursor:
+        data.append(item)
+    return data
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/signup/<data>',methods=['POST', 'GET'])
+def signup(data):
+    assert data == request.view_args['data']
+    if data=='doc':
+        cursor.execute("Select * from category where cat_parent='doctor'")
+        category=extractData(cursor)
+        return render_template('doc_register.html',cat=category)
+    if data=='tutor':
+        cursor.execute("Select * from category where cat_parent='tutor'")
+        category=extractData(cursor)
+        return render_template('doc_register.html',cat=category)
+    if data=='coder':
+        cursor.execute("Select * from category where cat_parent='coder'")
+        category=extractData(cursor)
+        return render_template('doc_register.html',cat=category)
+    if data == 'user':
+        cursor.execute("Select * from category")
+        category = extractData(cursor)
+        return render_template('user_register.html',cat=category)
+@app.route('/register/<data>',methods=['POST','GET'])
+def register(data):
+    assert data == request.view_args['data']
+    if data=='va':
+        va_id = request.form['va_id']
+        va_name = request.form['va_name']
+        va_address = request.form['va_address']
+        va_categories=request.form.getlist('categories')
+        #print(va_categories)
+        #print(request.form.getlist('categories'))
+        va_day=request.form['day']
+        va_start_hr=request.form['start_time_hr']
+        va_start_min = request.form['start_time_min']
+        va_end_hr = request.form['end_time_hr']
+        va_end_min = request.form['end_time_min']
+        cursor.execute("Insert into virtual_assistant Values(%s,%s,%s)",(va_id,va_name,va_address))
+        conn.commit()
+        cursor.execute("Insert into schedule Values(%s,%s,%s,%s,%s,%s)", (va_id,va_day,va_start_hr,va_start_min ,va_end_hr,va_end_min))
+        conn.commit()
+        for category in va_categories:
+            cursor.execute("Insert into specialization Values(%s,%s)",(va_id,category))
+            conn.commit()
 
+    elif data=='user':
+        va_categories = request.form.getlist('categories')
+        user_id = request.form['u_id']
+        user_name = request.form['user_name']
+        cursor.execute("Select * from User where u_id=%s",user_id)
+        if cursor.rowcount<1:
+            cursor.execute("Insert into User Values(%s,%s)", (user_id, user_name))
+            conn.commit()
+        query_result=[]
+        for i in va_categories:
+            query_get_numbers=("Select v.va_id,sc.va_day,sc.start_hr,sc.start_min ,sc.end_hr,sc.end_min from virtual_assistant v, specialization s,schedule sc where s.cat_id=%s and v.va_id=s.va_id and sc.va_id=v.va_id")
+            cursor.execute(query_get_numbers,(i))
+            query_result.append(extractData(cursor))
+        numbers=[]
+        now = datetime.datetime.now()
+        for i in query_result:
+            for j in i:
+                numbers.append(j)
+        numbers=set(numbers)
+        helpers_to_call=[]
+        for i in numbers:
+            start=now.replace(hour=i[2], minute=i[3], second=0, microsecond=0)
+            end=now.replace(hour=i[4], minute=i[5], second=0, microsecond=0)
+            if i[1]==time.strftime("%a"):
+                if now>start and now<end:
+                    helpers_to_call.append(i[0])
+        print(user_id)
+        print(helpers_to_call)
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
---
+++ BostonHacks.sql
@@ -0,0 +1,144 @@
+CREATE DATABASE IF NOT EXISTS `BostonHacks` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `BostonHacks`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL  AUTO_INCREMENT,
+  `cat_name` text NOT NULL,
+  `cat_parent` text NOT NULL,
+  Primary Key(cat_id)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+DROP TABLE IF EXISTS `virtual_assistant`;
+CREATE TABLE `virtual_assistant` (
+  `va_id` varchar(14) NOT NULL,
+  `va_name` text NOT NULL,
+  `va_address` text NOT NULL,
+  Primary key(va_id)
+) ;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `u_id` varchar(14) NOT NULL,
+  `name` text NOT NULL,
+  PRIMARY KEY (u_id)
+);
+
+--
+-- Table structure for table `problems`
+--
+
+DROP TABLE IF EXISTS `problems`;
+CREATE TABLE `problems` (
+  `p_id` int(11) NOT NULL  AUTO_INCREMENT,
+  `u_id` varchar(14) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `details` text NOT NULL,
+  `va_id` varchar(14) NOT NULL,
+  `va_input` text NOT NULL,
+  Primary key(p_id),
+  Foreign key(u_id) REFERENCES User(u_id),
+  Foreign key(va_id) REFERENCES virtual_assistant(va_id),
+  Foreign key(cat_id) REFERENCES category(cat_id)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialization`
+--
+
+DROP TABLE IF EXISTS `specialization`;
+CREATE TABLE `specialization` (
+  `va_id` varchar(14) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  Primary key(va_id,cat_id),
+  Foreign key(va_id) REFERENCES virtual_assistant(va_id),
+    Foreign key(cat_id) REFERENCES category(cat_id)
+) ;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table 'schedule'
+--
+DROP TABLE IF EXISTS `schedule`;
+CREATE TABLE `schedule` (
+  `va_id` varchar(14) NOT NULL,
+  `va_day` varchar(4) NOT NULL,
+  `start_hr` int(2) NOT NULL,
+  `start_min` int(2) NOT NULL,
+  `end_hr` int(2) NOT NULL,
+  `end_min` int(2) NOT NULL,
+  PRIMARY KEY(va_id,va_day),
+  Foreign key(va_id) REFERENCES virtual_assistant(va_id)
+);
+Insert into virtual_assistant Values('+16179552483','John Einstein','1167 Boylston Street,Boston, MA 02215');
+Insert into virtual_assistant Values('+18572058079','Ruturaj Nene','1171 Boylston Street,Boston, MA 02215');
+Insert into virtual_assistant Values('+919987874666','Pranav Raikundalia','Malad, Mumbai 400064');
+
+Insert into category(cat_name,cat_parent) Values('Orthopedic','doctor');
+Insert into category(cat_name,cat_parent) Values('General','doctor');
+Insert into category(cat_name,cat_parent) Values('Cardiologist','doctor');
+Insert into category(cat_name,cat_parent) Values('Pediatrician','doctor');
+Insert into category(cat_name,cat_parent) Values('Maternity Care','doctor');
+Insert into category(cat_name,cat_parent) Values('Psychiatrist','doctor');
+Insert into category(cat_name,cat_parent) Values('Chemistry','tutor');
+Insert into category(cat_name,cat_parent) Values('Biology','tutor');
+Insert into category(cat_name,cat_parent) Values('Physics','tutor');
+Insert into category(cat_name,cat_parent) Values('Maths','tutor');
+Insert into category(cat_name,cat_parent) Values('Computer Science','tutor');
+Insert into category(cat_name,cat_parent) Values('History','tutor');
+Insert into category(cat_name,cat_parent) Values('Economics','tutor');
+Insert into category(cat_name,cat_parent) Values('Java','coder');
+Insert into category(cat_name,cat_parent) Values('C/C++','coder');
+Insert into category(cat_name,cat_parent) Values('Python','coder');
+Insert into category(cat_name,cat_parent) Values('R','coder');
+Insert into category(cat_name,cat_parent) Values('Rackets/Scheme','coder');
+Insert into category(cat_name,cat_parent) Values('MySQL','coder');
+Insert into category(cat_name,cat_parent) Values('PHP','coder');
+Insert into category(cat_name,cat_parent) Values('Shell','coder');
+
+
+
+Insert into specialization Values('+16179552483','1');
+Insert into specialization Values('+16179552483','2');
+Insert into specialization Values('+16179552483','3');
+Insert into specialization Values('+16179552483','4');
+Insert into specialization Values('+16179552483','5');
+Insert into specialization Values('+16179552483','6');
+Insert into specialization Values('+18572058079','7');
+Insert into specialization Values('+18572058079','8');
+Insert into specialization Values('+18572058079','9');
+Insert into specialization Values('+18572058079','10');
+Insert into specialization Values('+18572058079','11');
+Insert into specialization Values('+18572058079','12');
+Insert into specialization Values('+18572058079','13');
+Insert into specialization Values('+919987874666','14');
+Insert into specialization Values('+919987874666','15');
+Insert into specialization Values('+919987874666','16');
+Insert into specialization Values('+919987874666','17');
+Insert into specialization Values('+919987874666','18');
+Insert into specialization Values('+919987874666','19');
+Insert into specialization Values('+919987874666','20');
+Insert into specialization Values('+919987874666','21');
+
+Insert into schedule Values('+16179552483','Sun','00','00','05','00');
+Insert into schedule Values('+18572058079','Sun','10','00','17','00');
+Insert into schedule Values('+919987874666','Sun','10','00','17','00');
